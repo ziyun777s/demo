81a6b5362917e88210890e97d5f7de98941fea97
ziyun <286438197@qq.com>
2020-08-18 15:30:35 +0800

    ignore

--- hello.java
+++ hello.java
@@ -1,8 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World10");
-        System.out.println("Hello World20");
-        System.out.println("World3");
-        System.out.println("World4");
+        System.out.println("Hello World1");
+        System.out.println("Hello World！");
     }
 }
