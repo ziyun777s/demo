caf140ff941132eeafadd02f1a5a270b358258c6
ziyun <286438197@qq.com>
2020-08-18 13:55:30 +0800

    “hello java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
+        System.out.println("Hello World4");
+    }
+}
