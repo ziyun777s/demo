c34213a5c89d43d4a15b39aa57e2f0ee4cf9d365
ziyun <286438197@qq.com>
2020-08-18 14:40:56 +0800

    updata

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World10");
-        System.out.println("Hello World20");
-        System.out.println("Hello World30");
-        System.out.println("Hello World40");
+        System.out.println("Hello World！");
+        System.out.println("Hello World！");
+        System.out.println("Hello World！");
+        System.out.println("Hello World！");
     }
 }
