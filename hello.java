a3e1f138d924abf40bd48a3ac4c5f8db680d1f1d
ziyun <286438197@qq.com>
2020-08-18 14:09:10 +0800

    fix hello java

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World1");
-        System.out.println("Hello World2");
-        System.out.println("Hello World3");
-        System.out.println("Hello World4");
+        System.out.println("Hello World10");
+        System.out.println("Hello World20");
+        System.out.println("Hello World30");
+        System.out.println("Hello World40");
     }
 }
